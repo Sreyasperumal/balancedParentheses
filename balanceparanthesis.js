33195dc582c012ffba778a092cbb8b40ab07b15d
Sreyasperumal <136047382+Sreyasperumal@users.noreply.github.com>
2023-06-14 05:03:25 +0100

    upload balanceparanthesis.js

---
+++ balanceparanthesis.js
@@ -0,0 +1,43 @@
+function areParenthesesBalanced(expr)
+{
+let stack=[];
+for(i=0;i<expr.length;i++)
+{
+    let x=expr[i];
+    if(x=='('||x=='{'||x=='[')
+    {
+        stack.push(x);
+        continue;
+    }
+    if(stack.length ==0)
+    return false;
+let check;
+switch (x){
+    case ')':
+     check=stack.pop();
+     if(check == '{'||check == '[')
+     return false;
+     break;
+     case '}':
+     check=stack.pop();
+     if(check == '('||check == '[')
+     return false;
+     break;
+     case ']':
+     check=stack.pop();
+     if(check == '('||check == '{')
+     return false;
+     break;
+}
+}
+return(stack.length ==0);
+
+}
+let expr ="([{}]))";
+if(areParenthesesBalanced(expr)){
+
+     console.log("yes");
+}
+else{
+    console.log("no");
+}
